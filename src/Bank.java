86aa1cbdf750e9d52985be79545de4bd6fab6b42
John Cletus <jhornelcle@gmail.com>
2025-05-26 15:34:36 +0100

    Working on creating private classes with a banking system, completed setting up the setters and the getters

--- src/Bank.java
+++ src/Bank.java
@@ -1,2 +1,59 @@
 public class Bank {
+
+    private int accountNumber;
+    private String accountType;
+    private String accountName;
+    private double balance;
+    private String phoneNumber;
+    private String email;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 }
