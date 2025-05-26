af3e4a5aaf153d63771122df676dfbc0bd80e8c9
John Cletus <jhornelcle@gmail.com>
2025-05-26 10:19:42 +0100

    Java studies initial application reading inputs

--- src/readingInputs.java
+++ src/readingInputs.java
@@ -1,2 +1,20 @@
 public class readingInputs {
+    public static void main(String[] args) {
+        int currentYear = 2025;
+
+        System.out.println(getInputFromConsole(currentYear));
+        System.out.println(getInputFromScanner(currentYear));
+    }
+
+    public static String getInputFromConsole(int userAge) {
+        String name = System.console().readLine("Please enter your name: ");
+        System.out.println("Hi " + name + " thank you for joining us today!");
+
+        return "";
+    }
+
+
+    public static String getInputFromScanner(int userAge) {
+        return "";
+    }
 }
