c5498f9feda18afdf4188e875ccd9e7defd486dc
John Cletus <jhornelcle@gmail.com>
2025-05-28 13:30:28 +0100

    array studies - combining arrays with objects

--- src/PersonalArrays.java
+++ src/PersonalArrays.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PersonalArrays {
@@ -7,9 +8,20 @@ public class PersonalArrays {
         int[] newArray = new int[10];
         String[] newStringArray = {"boy", "girl", "man", "woman"};
 
+        ArrayList<ClassMan> Citizens = new ArrayList<>();
+
+        Citizens.add(new ClassMan("John", 25, "Chocolate"));
+        Citizens.add(new ClassMan("Niko", 24, "Chocolate"));
+        Citizens.add(new ClassMan("Domo", 25, "Dark"));
+
+
+        for (ClassMan citizen : Citizens) {
+            System.out.println(citizen);
+        }
+
 
         for (int i = 0; i < newArray.length; i++) {
-            newArray[i] = (int)(Math.random() * 10);
+            newArray[i] = (int) (Math.random() * 10);
         }
         System.out.println(Arrays.toString(newArray));
         System.out.println(Arrays.toString(newStringArray));
